02d365b1a980ed92fdcb90bc0a0a6a75fd1d9535
ruler_korara <huiseong4132@gmail.com>
2025-06-09 23:04:57 +0900

    방 완료

--- Dog cat Soup v2/Dog cat Soup V2.c
+++ Dog cat Soup v2/Dog cat Soup V2.c
@@ -56,6 +56,8 @@ int main() {
     int mood = 3;
     int hasScratcher = 0;
     int hasTower = 0;
+    int SCR_POS = -1;
+	int TWR_POS = -1;
 
     printMainScreen();
     printf("야옹이의 이름은 개냥이입니다.\n");
@@ -83,52 +85,99 @@ int main() {
 
 			prevCatPosition = catPosition;
 
-        int renderRoom(int catPosition, int prevCatPosition); {
-            printf("\n");
-            for (int i = 0; i < ROOM_WIDTH + 2; i++) {
-                printf("#");
+            if (mood == 0) {
+                if (catPosition > HOME_POS) {
+                    catPosition = catPosition - 1;
+                    printf("기분이 매우 나쁜 쫀떡은 집으로 돌아갑니다.\n");
+                }
             }
-            printf("\n#H");
-            for (int i = 0; i < ROOM_WIDTH - 2; i++) {
-                printf(" ");
-            }
-            printf("B#\n");
+            else if (mood == 1) {
+                if (hasScratcher == 1 || hasTower == 1) {
+                    int distScratcher = ROOM_WIDTH - 2;
+                    int distTower = ROOM_WIDTH - 2;
 
-            printf("#");
-            for (int i = 0; i < ROOM_WIDTH; i++) {
-                if (i == catPosition) {
-                    printf("C");
-                }
-                else if (prevCatPosition < catPosition && i == catPosition - 1) {
-                    printf("."); // 오른쪽 이동 시 왼쪽에 점
-                }
-                else if (prevCatPosition > catPosition && i == catPosition + 1) {
-                    printf("."); // 왼쪽 이동 시 오른쪽에 점
+                    if (hasScratcher == 1) {
+                        if (SCR_POS > catPosition)  distScratcher = SCR_POS - catPosition;
+                        else distScratcher = catPosition - SCR_POS;
+                    }
+                    if (hasTower == 1) {
+                        if (TWR_POS > catPosition) distTower = TWR_POS - catPosition;
+                        else distTower = catPosition - TWR_POS;
+                    }
+
+                    int target;
+                    if (distScratcher <= distTower) target = SCR_POS;
+                    else target = TWR_POS;
+
+                    if (catPosition < target) catPosition = catPosition + 1;
+                    else if (catPosition > target) catPosition = catPosition - 1;
+
+                    if (target == SCR_POS) printf("개냥이는 심심해서 스크래처 쪽으로 이동합니다.\n");
+                    else printf("개냥이는 심심해서 캣타워 쪽으로 이동합니다.\n");
                 }
                 else {
-                    printf(" ");
+                    mood = mood - 1;
+                    printf("놀 거리가 없어서 기분이 매우 나빠집니다.\n");
                 }
+            }
+            else if (mood == 2) {
+                printf("개냥이는 기분좋게 식빵을 굽고 있습니다.\n");
+            }
+            else if (mood == 3) {
+                if (catPosition < BOWL_POS) {
+                    catPosition = catPosition + 1;
+                    printf("개냥이는 골골송을 부르며 수프를 만들러 갑니다.\n");
+                }
+            }
+            if (catPosition == HOME_POS && prevCatPosition == HOME_POS) {
+                if (mood < 3) {
+                    mood = mood + 1;
+                    printf("개냥이는 집에서 휴식을 취하며 기분이 좋아졌습니다.\n");
+                }
+            }
+            else if (catPosition == BOWL_POS && prevCatPosition == BOWL_POS) {
+                int soupType = rand() % 3;
+                if (soupType == 0) printf("개냥이가 감자 수프를 만들었습니다!\n");
+                else if (soupType == 1) printf("개냥이가 양송이 수프를 만들었습니다!\n");
+                else printf("개냥이가 브로콜리 수프를 만들었습니다!\n");
+                soupCount++;
+                printf("현재까지 만든 수프: %d개\n", soupCount);
+            }
+            else if (hasScratcher == 1 && SCR_POS != -1 && catPosition == SCR_POS && prevCatPosition != SCR_POS) {
+                if (mood < 3) mood++;
+                printf("개냥이가 스크래처를 긁고 놀았습니다.\n");
+            }
+            else if (hasTower == 1 && TWR_POS != -1 && catPosition == TWR_POS && prevCatPosition != TWR_POS) {
+                if (mood <= 1) mood += 2;
+                else mood = 3;
+                printf("개냥이가 캣타워를 뛰어다닙니다.\n");
+            }
+
+            for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
+            printf("\n");
+
+            printf("#");
+            for(int i = 1; i<ROOM_WIDTH - 1; i++) {
+                if (i == HOME_POS) printf("H");
+                else if (i == BOWL_POS) printf("B");
+                else if (hasScratcher == 1 && i == SCR_POS && SCR_POS != -1) printf("S");
+                else if (hasTower == 1 && i == TWR_POS && TWR_POS != -1) printf("T");
+                else printf(" ");
+			}
+
+            printf("#\n");
+			printf("#");
+            for (int i = 1; i < ROOM_WIDTH - 1; i++) {
+                if (i == catPosition) printf("C");
+                else if (i == prevCatPosition && catPosition != prevCatPosition) printf(".");
+                else printf(" ");
             }
             printf("#\n");
 
-            for (int i = 0; i < ROOM_WIDTH + 2; i++) {
-                printf("#");
-            }
-            printf("\n");
-        }
+            for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
+            printf("\n\n");
 
-        if (catPosition == HOME_POS) {
-            printf("개냥이는 자신의 집에서 편안함을 느낍니다.\n");
-        }
 
-        if (catPosition == BOWL_POS) {
-            int soupType = rand() % 3;
-            if (soupType == 0) printf("개냥이가 감자 수프를 만들었습니다!\n");
-            else if (soupType == 1) printf("개냥이가 양송이 수프를 만들었습니다!\n");
-            else printf("개냥이가 브로콜리 수프를 만들었습니다!\n");
-            soupCount++;
-            printf("현재까지 만든 수프: %d개\n", soupCount);
-        }
 
         printf("\n어떤 상호작용을 하시겠습니까? 0. 아무것도 하지 않음 1. 긁어 주기\n");
 
